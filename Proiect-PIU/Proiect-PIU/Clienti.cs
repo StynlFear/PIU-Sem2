bd006a3d99adc021d5fd7c5018970de5b23a1c80
StynlFear <82573820+StynlFear@users.noreply.github.com>
2023-03-15 09:51:46 +0200

    [ADD]- First Commit

---
+++ Proiect-PIU/Proiect-PIU/Clienti.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Proiect_PIU
+{
+    public class Clienti
+    {
+
+        //	Constructor fara parametri
+        public void Afisare()
+        {
+            Console.WriteLine("Denumire");
+        }
+
+        //	Constructor cu parametri
+       
+
+        //	Metoda care returneaza informatiile despre figura geometrica 
+        //	sub forma unui sir de caractere
+       
+    }
+}
